02f258688914be0ad528ed09262d6fa79a891b9b
Harold Pinto <harolpintosuarez54321@gmail.com>
2025-05-06 17:10:44 -0500

    Pagina

---
+++ pb/src/pages/Inicio/inico.jsx
@@ -0,0 +1,12 @@
+import './inicio.css';
+
+function Inicio() {
+  return (
+    <main className="inicio-main">
+      <h1>Bienvenido a la página de inicio</h1>
+      <p>Esta es la página principal de la aplicación.</p>
+    </main>
+  );
+}
+
+export default Inicio;
